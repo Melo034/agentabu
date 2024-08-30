662013f251f308d546908d9182178d5e6161ede0
Joseph Melvin Kanu <kanujosephmelvin@gmail.com>
2024-08-30 21:56:40 +0000

    update

--- Includes/header.php
+++ Includes/header.php
@@ -10,8 +10,6 @@
 <!-- Alpine.js for Reactive Components -->
 <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-<!-- Typed.js for Typing Animations -->
-<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
 <!-- Animate.css for CSS Animations -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 <!-- Google Fonts Preconnect -->
